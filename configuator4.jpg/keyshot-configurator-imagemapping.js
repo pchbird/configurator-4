3d1510885e6f2c6eb51080088ea8a681a9ecb8a2
pchbird <61976121+pchbird@users.noreply.github.com>
2020-04-30 16:18:02 -0400

    images
    images

---
+++ configuator4.jpg/keyshot-configurator-imagemapping.js
@@ -0,0 +1,120 @@
+imagemappingdata =
+[
+    {
+        "image": "configuator test 3.220.1.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "10+5",
+            "15+11",
+            "20+16",
+            "24+21"
+        ],
+        "studio": 67608
+    },
+    {
+        "image": "configuator test 3.220.2.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "10+7",
+            "15+12",
+            "20+17",
+            "24+22"
+        ],
+        "studio": 67608
+    },
+    {
+        "image": "configuator test 3.220.3.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "10+6",
+            "15+13",
+            "20+16",
+            "24+21"
+        ],
+        "studio": 67608
+    },
+    {
+        "image": "configuator test 3.220.4.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "10+5",
+            "15+11",
+            "20+16",
+            "24+21"
+        ],
+        "studio": 67619
+    },
+    {
+        "image": "configuator test 3.220.5.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "10+7",
+            "15+12",
+            "20+17",
+            "24+22"
+        ],
+        "studio": 67619
+    },
+    {
+        "image": "configuator test 3.220.6.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "10+6",
+            "15+13",
+            "20+16",
+            "24+21"
+        ],
+        "studio": 67619
+    },
+    {
+        "image": "configuator test 3.220.7.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "10+5",
+            "15+11",
+            "20+16",
+            "24+21"
+        ],
+        "studio": 67607
+    },
+    {
+        "image": "configuator test 3.220.8.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "10+7",
+            "15+12",
+            "20+17",
+            "24+22"
+        ],
+        "studio": 67607
+    },
+    {
+        "image": "configuator test 3.220.9.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "10+6",
+            "15+13",
+            "20+16",
+            "24+21"
+        ],
+        "studio": 67607
+    }
+];
---
+++ configuator4.jpg/keyshot-configurator-modeldata.js
@@ -0,0 +1,70 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "checked": true,
+                    "icon": "traditional%20.png",
+                    "multimaterials": [
+                        "15+11",
+                        "20+16",
+                        "10+5",
+                        "24+21"
+                    ],
+                    "name": "traditional "
+                },
+                {
+                    "icon": "funky.png",
+                    "multimaterials": [
+                        "15+12",
+                        "20+17",
+                        "10+7",
+                        "24+22"
+                    ],
+                    "name": "funky"
+                },
+                {
+                    "icon": "worm.png",
+                    "multimaterials": [
+                        "15+13",
+                        "20+16",
+                        "10+6",
+                        "24+21"
+                    ],
+                    "name": "worm"
+                }
+            ],
+            "name": "color variations"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "icon": "topview.png",
+            "materialgroups": [
+                "color variations"
+            ],
+            "modelset": 1005,
+            "name": "topview"
+        }
+    ],
+    "studios": [
+        {
+            "checked": true,
+            "icon": "specifications.png",
+            "id": 67619,
+            "name": "specifications"
+        },
+        {
+            "icon": "perspective.png",
+            "id": 67607,
+            "name": "perspective"
+        },
+        {
+            "icon": "top%20view.png",
+            "id": 67608,
+            "name": "top view"
+        }
+    ]
+};
